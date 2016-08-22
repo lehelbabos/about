db048c9a673816e40b51a88a39832d69dfff6c7a
Lehel <contact@lehelbabos.com>
2016-08-21 21:48:05 -0400

    Configured SASS compilation in Gulp. Added and chained in autoprefixer. Modified gitignore file to ignore dist dir.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,40 @@
+'use strict';
+
+//////////////////////////////
+// Requires
+//////////////////////////////
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', defaultTask);
+//////////////////////////////
+// Directories
+//////////////////////////////
 
-function defaultTask(done) {
-  // place code for your default task here
-  done();
-}
+var dirs = {
+  'sass': ['src/app/**/*.scss','src/sass/**/*.scss' ],
+  'dist': 'dist/'
+};
+
+
+//////////////////////////////
+// Sass Tasks
+//////////////////////////////
+gulp.task('sass', function () {
+  gulp.src(dirs.sass)
+    .pipe(sass({
+      'outputStyle': 'expanded',
+    })
+      .on('error', sass.logError) // This keeps the rest of the Gulp tasks from stopping if there is an error
+    )
+    .pipe(autoprefixer()) // Run output through autoprefixer
+    .pipe(gulp.dest(dirs.dist + 'css')) // Spit out CSS
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch(dirs.sass, ['sass']);
+});
+
+
+
+gulp.task('watch', ['sass:watch']);
