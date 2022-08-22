866f4949992d6bfaa264e401d5f1d90ea41dc3b8
Lehel Babos <contact@lehelbabos.com>
2022-08-21 22:56:35 -0400

    Fixed ghost

--- posts/versions/4.4.0/core/server/api/v3/utils/serializers/output/oembed.js
+++
@@ -1,8 +0,0 @@
-const debug = require('ghost-ignition').debug('api:v3:utils:serializers:output:oembed');
-
-module.exports = {
-    all(res, apiConfig, frame) {
-        debug('all');
-        frame.response = res;
-    }
-};
